5748b8df251685d0f40e854c9b79d6b47853ad26
Alexander Alcantara Linhares <alex.linharesalcantara@gmail.com>
2025-03-14 15:32:47 -0300

    Adicionando database ao projeto e iniciando manipulação de dados em using_pandas.py

--- using_pandas.py
+++ using_pandas.py
@@ -1,19 +1,3 @@
 import plotly.express as px
 import pandas as pd
 
-df = pd.DataFrame({
-    "Nomes": ["Walter", "Marcelo", "Roberto", "Carlos", "Antônio", "José", "Maria"],
-    "Idades": [20, 23, 40, 12, 45, 80, 7],
-    "Medias": [6.3, 7, 6.0, 8.9, 10, 5.7, 9.2]
-})
-
-fig = px.bar(
-    df, 
-    x='Idades',
-    y='Medias', 
-    text='Nomes'
-)
-
-#aí vc descobre que não tá entendo nada 
-
-fig.show()
